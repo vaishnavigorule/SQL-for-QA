bde463920b6972a22f50f91e4eaaa943768d46a1
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-04 11:16:59 +0530

    Create Employee_Table.sql

---
+++ Employee_Table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    DepartmentID INT,
+    Salary INT
+);
+
+INSERT INTO Employee VALUES
+(1,'Rahul',101,50000),
+(2,'Priya',102,60000),
+(3,'Amit',101,45000),
+(4,'Neha',103,70000),
+(5,'Karan',NULL,40000);
