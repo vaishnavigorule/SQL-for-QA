b0c9c40804beeabd86af198026ab1be86df222da
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-04 11:17:31 +0530

    Create Department_Table.sql

---
+++ Department_Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50)
+);
+
+INSERT INTO Department VALUES
+(101,'IT'),
+(102,'HR'),
+(103,'Finance'),
+(104,'Marketing');
